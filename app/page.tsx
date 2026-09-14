3f6b64a4108f5218f589a060e769e0c1ff0d67fa
praveenwelgama <welgamapraveen@gmail.com>
2026-09-14 22:34:59 +0530

    mikssing parts

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,17 @@ import CartBar from '@/components/CartBar';
 import ItemModal from '@/components/ItemModal';
 import OrderModal from '@/components/OrderModal';
 import Navbar from '@/components/Navbar';
+import Promos from '@/components/Promos';
+import Contact from '@/components/Contact';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#070707]">
       <Navbar />
       <Hero />
+      <Promos />
       <MenuGrid />
+      <Contact />
       <CartBar />
 
       {/* Wrap searchParams modals in Suspense boundaries for Next.js build */}
